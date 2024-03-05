9fbf1144c95d47ad69f36180dd87d64006c8504a
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-03-05 12:53:19 +0900

    [feat] create header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="w-full flex justify-center">
+      <header className="w-4/5 h-12 flex gap-12 justify-center items-center border-b-2">
+        <nav>
+          <Link href="/aboutme">About Me</Link>
+        </nav>
+        <nav>
+          <Link href="/work">Work</Link>
+        </nav>
+        <nav>
+          <Link href="/project">Project</Link>
+        </nav>
+        <nav>
+          <Link href="/blog">Blog</Link>
+        </nav>
+      </header>
+    </div>
+  );
+}
