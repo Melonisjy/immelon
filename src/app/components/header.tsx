f310dd31f9a40907d5474ba14726f883de66c988
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-03-08 12:57:06 +0900

    [feat] header menu 추가

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,19 +3,23 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="w-full flex justify-center">
-      <header className="w-4/5 h-16 flex gap-20 justify-center items-center border-b-2">
-        <nav>
-          <Link href="/aboutme">About Me</Link>
-        </nav>
-        <nav>
-          <Link href="/work">Work</Link>
-        </nav>
-        <nav>
-          <Link href="/project">Project</Link>
-        </nav>
-        <nav>
-          <Link href="/blog">Blog</Link>
-        </nav>
+      <header className="w-full h-16 flex justify-between items-center border-b-2 mx-12">
+        <div className="text-xl">Lee Joo Yeol</div>
+        <section className="flex gap-20">
+          <nav>
+            <Link href="/aboutme">About Me</Link>
+          </nav>
+          <nav>
+            <Link href="/work">Work</Link>
+          </nav>
+          <nav>
+            <Link href="/project">Project</Link>
+          </nav>
+          <nav>
+            <Link href="/blog">Blog</Link>
+          </nav>
+        </section>
+        <div>bla bla</div>
       </header>
     </div>
   );
