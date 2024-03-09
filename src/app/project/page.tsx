aa57a49c2c994c1165c779d66eecb4b6ec9a2d69
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-03-09 17:41:11 +0900

    [feat] project page added

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,56 @@
-import P from "../index.module.css";
+// import P from "../index.module.css";
 
 function Project() {
-  return <div className={P.page}>Project</div>;
+  return (
+    <div>
+      <section className="h-72 flex justify-center items-center bg-slate-200 ">
+        <h1 className="text-7xl font-bold">Explore My Project</h1>
+      </section>
+      <section className="grid grid-cols-1">
+        <article className="rounded-lg mt-20 mx-4 bg-slate-400">
+          <div className="py-20 pl-4 w-2/5 min-h-96 flex flex-col justify-between">
+            <div className="text-4xl">INSURSAND</div>
+            <div>
+              INSURSAND revolutionizes access to insurance in DeFi with two
+              offerings: asset and staking insurance. Operating on a test
+              network, we secure crypto assets, transforming the DeFi insurance
+              landscape, and empowering users to explore the digital economy
+              confidently.
+            </div>
+            <button className="flex justify-end">{`More about project ->`}</button>
+          </div>
+          <div></div>
+        </article>
+        <article className="rounded-lg mt-20 mx-4 bg-slate-400">
+          <div className="py-20 pl-4 w-2/5 min-h-96 flex flex-col justify-between">
+            <div className="text-4xl">INSURSAND</div>
+            <div>
+              INSURSAND revolutionizes access to insurance in DeFi with two
+              offerings: asset and staking insurance. Operating on a test
+              network, we secure crypto assets, transforming the DeFi insurance
+              landscape, and empowering users to explore the digital economy
+              confidently.
+            </div>
+            <button className="flex justify-end">{`More about project ->`}</button>
+          </div>
+          <div></div>
+        </article>
+        <article className="rounded-lg mt-20 mx-4 bg-slate-400">
+          <div className="py-20 pl-4 w-2/5 min-h-96 flex flex-col justify-between">
+            <div className="text-4xl">INSURSAND</div>
+            <div>
+              INSURSAND revolutionizes access to insurance in DeFi with two
+              offerings: asset and staking insurance. Operating on a test
+              network, we secure crypto assets, transforming the DeFi insurance
+              landscape, and empowering users to explore the digital economy
+              confidently.
+            </div>
+            <button className="flex justify-end">{`More about project ->`}</button>
+          </div>
+          <div></div>
+        </article>
+      </section>
+    </div>
+  );
 }
 export default Project;
