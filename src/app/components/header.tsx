544b9e341340189f977f60c06b9b360a1d176296
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-03-09 17:40:54 +0900

    [feat] scroll event addded

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,37 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [scrollY, setScrollY] = useState<number>();
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    setScrollY(scrollY);
+  };
+
   return (
-    <div className="w-full flex justify-center">
-      <header className="w-full h-16 flex justify-between items-center border-b-2 mx-12">
-        <div className="text-xl">Lee Joo Yeol</div>
+    <div
+      className={`w-full justify-center ${
+        scrollY && scrollY > 0 ? "bg-black" : "bg-transparent"
+      } fixed`}
+    >
+      <header
+        className={`w-full h-16 flex justify-between items-center ${
+          scrollY && scrollY > 0
+            ? "text-white"
+            : "border-b-slate-400 border-b-2"
+        } px-12`}
+      >
+        <div className="text-xl select-none">Lee Joo Yeol</div>
         <section className="flex gap-20">
           <nav>
             <Link href="/aboutme">About Me</Link>
