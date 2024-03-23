546e489f7dd4147929175df2e7a31037f5abe424
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-03-23 15:59:49 +0900

    [feat] develop detail page

--- src/app/project/[projectUrl]/page.tsx
+++ src/app/project/[projectUrl]/page.tsx
@@ -1,15 +1,19 @@
-"use client";
-
-import { useParams } from "next/navigation";
-
-const Detail = () => {
-  const project = useParams();
+interface DetailProps {
+  params: {
+    projectUrl: string;
+  };
+}
+const Detail = (props: DetailProps) => {
+  const projectName: string = props.params.projectUrl;
 
   return (
-    <section className="h-full mt-24 w-full bg-slate-400 flex justify-center items-center">
-      <h1 className="uppercase text-7xl font-bold text-slate-700">
-        {project.projectUrl}
-      </h1>
+    <section className="h-full w-full bg-slate-400 flex justify-center">
+      <div className="w-full flex justify-around">
+        <section></section>
+        <h1 className="uppercase text-7xl font-bold text-slate-700 flex justify-center">
+          {projectName}
+        </h1>
+      </div>
     </section>
   );
 };
