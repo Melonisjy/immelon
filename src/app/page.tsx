6e3f639ce892519347e24f4bbedd11ca6c183f44
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-04-01 18:40:43 +0900

    [feat] home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 export default function Home() {
   return (
     <div className="h-full pt-44 flex justify-center flex-col gap-6 items-center">
-      <div className="bg-slate-300 p-3 rounded-xl text-xl">
-        Hello! I’m Melon, and i create...
+      <div className="bg-slate-300 p-3 rounded-xl tracking-tighter text-xl">
+        Hello! I’m Melon, and I’m...
       </div>
-      <div className="text-7xl text-pretty text-center leading-tight">
+      <div className="text-7xl text-pretty text-center tracking-tight leading-snug">
         Outstanding websites <br /> crafted with Webflow
+      </div>
+      <div className="text-lg tracking-wider text-center leading-5">
+        Webflow development tailored for design-focused companies,
+        <br /> emphasizing meticulous attention to detail and unwavering
+        passion.
       </div>
     </div>
   );
